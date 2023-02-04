681cb6fa3becfdf658ccca46a07befd7f5af6490
Vutomi Luigi Khosa <khosavutomi99@gmail.com>
2023-02-04 09:49:44 +0200

    Update apiSlice.js

--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:3500' || 'https://notes-api-6q23.onrender.com',
+    baseUrl: 'https://notes-api-6q23.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
